7ac229a17acf114b6b0705aabf798608c979b1b3
= <=>
2021-09-06 22:10:15 +0300

    create file HW

---
+++ HomeWork#1/script.js
@@ -0,0 +1,27 @@
+// Задание 1
+
+let personAge;
+personAge = "21";
+let personName; 
+personName = "Darya";
+let personSurname; 
+personSurname = "Panurova";
+
+// Задание 2
+
+let newUser;
+let shoppingList;
+let userOnline;
+let workerSalary;
+let coordinatesCursor;
+
+// Задание 3
+
+let firstVariable = "Hello World!";
+console.log(firstVariable);
+
+
+console.log( personName);
+
+
+
